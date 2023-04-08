26642fbbf23d1e83dfd762f05d47426f859d4fce
Chen-Hao-fc <chenhao_fc@163.com>
2023-04-08 17:21:15 +0800

    add script.sh

---
+++ scripts.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+
+python3  --method pet \
+         --pattern_ids 0 1 2 3 4 \
+         --data_dir data/ag_news_csv \
+         --model_type bert \
+         --model_name_or_path model/bert-base-uncased \
+         --task_name agnews \
+         --output_dir output/agnews \
+         --do_train
