c461a97f9a9758cfb94305da9b1938af10d8ef41
Chen-Hao-fc <chenhao_fc@163.com>
2023-04-08 17:24:04 +0800

    +

--- scripts.sh
+++ scripts.sh
@@ -2,11 +2,12 @@
 
 
 
-python3  --method pet \
-         --pattern_ids 0 1 2 3 4 \
-         --data_dir data/ag_news_csv \
-         --model_type bert \
-         --model_name_or_path model/bert-base-uncased \
-         --task_name agnews \
-         --output_dir output/agnews \
-         --do_train
+python3 cli.py \
+ --method pet \
+ --pattern_ids 0 1 2 3 4 \
+ --data_dir data/ag_news_csv \
+ --model_type bert \
+ --model_name_or_path model/bert-base-uncased \
+ --task_name agnews \
+ --output_dir output/agnews \
+ --do_train
