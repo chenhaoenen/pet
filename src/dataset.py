7f94f7763d17f11562cca1b1aead25ce3545bb87
Chen-Hao-fc <chenhao_fc@163.com>
2023-04-28 18:37:49 +0800

    +

---
+++ src/dataset.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-      
+# --------------------------------------------
+# Author: chen hao
+# Date: 2023/04/28 15:19
+# --------------------------------------------
+import csv
+from torch.utils.data import Dataset
+from transformers import AutoTokenizer, BertTokenizer
+
+
+
+class AgNewsDataset(Dataset):
+    VERBALIZER = {
+        "1": ["World"],
+        "2": ["Sports"],
+        "3": ["Business"],
+        "4": ["Tech"]
+    }
+    VERBALIZER_INDEX = {
+        "1": 0,
+        "2": 1,
+        "3": 2,
+        "4": 3
+    }
+
+    def __init__(self, data_path, model_path, pattern_id, max_length):
+        self.examples = []
+        self.tokenizer = BertTokenizer.from_pretrained(model_path) #加载tokenizer
+        self.mask = self.tokenizer.mask_token # 获取tokenizer中mask标签</mask>
+        self.mask_id = self.tokenizer.mask_token_id # 词表中</mask>对应的id
+        print(f'self.mask:{self.mask} self.mask_id:{self.mask_id}')
+
+        self.max_length = max_length
+        self.pattern_id = pattern_id
+        with open(data_path) as f:
+            reader = csv.reader(f, delimiter=',')
+            for idx, row in enumerate(reader):
+                label, headline, body = row
+                text_a = headline.replace('\\', ' ')
+                text_b = body.replace('\\', ' ')
+                if label not in self.VERBALIZER_INDEX: continue
+                example = [text_a, text_b, self.VERBALIZER_INDEX[label]]
+                self.examples.append(example)
+
+    def __len__(self):
+        return len(self.examples)
+
+    def encode(self, text_a, text_b):
+
+        if self.pattern_id == 0:
+            prompt_text =  [self.mask, ':', text_a, text_b]
+        elif self.pattern_id == 1:
+            prompt_text =  [self.mask, 'News:', text_a, text_b]
+        elif self.pattern_id == 2:
+            prompt_text =  [text_a, '(', self.mask, ')', text_b]
+        elif self.pattern_id == 3:
+            prompt_text = [text_a, text_b, '(', self.mask, ')']
+        elif self.pattern_id == 4:
+            prompt_text = ['[ Category:', self.mask, ']', text_a, text_b]
+        elif self.pattern_id == 5:
+            prompt_text = [self.mask, '-', text_a, text_b]
+        else:
+            raise ValueError("No pattern implemented for id {}".format(self.pattern_id))
+
+
+        feature = self.tokenizer(''.join(prompt_text),
+                                 add_special_tokens=False,
+                                 max_length=self.max_length,
+                                 padding='max_length',
+                                 return_tensors='pt')
+        return feature
+
+
+    def get_mlm_labels(self, input_ids):
+        print(input_ids)
+        label_idx = input_ids.index(self.mask_id)
+        labels = [-1] * len(input_ids)
+        labels[label_idx] = 1
+        return labels
+
+    def get_verbalizer(self, label):
+        return self.VERBALIZER[label]
+
+    def __getitem__(self, idx):
+        text_a, text_b, label = self.examples[idx]
+        feature = self.encode(text_a, text_b)
+
+        # get_mask_positions
+        input_ids = feature.input_ids.tolist()[0]
+        mlm_labels = self.get_mlm_labels(input_ids)
+
+        return feature, mlm_labels, label
+
+
+
+
+
+
---
+++ src/train.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-      
+# --------------------------------------------
+# Author: chen hao
+# Date: 2023/04/28 15:08
+# --------------------------------------------
+import time
+import torch
+from datetime import datetime
+from transformers import set_seed
+from torch.utils.tensorboard import SummaryWriter
+from src.dataset import AgNewsDataset
+from torch.utils.data import DataLoader
+
+
+class Config():
+    def __init__(self):
+        self.batch_size = 2048
+        self.epochs = 50
+        self.seed = 1234
+        self.learning_rate = 1.5e-5
+        self.log_freq = 100
+        self.eval_freq = 500
+        self.max_seq_length = 256
+        self.train_data_path = '../data/ag_news_csv/train.csv'
+        self.eval_data_path = '../data/ag_news_csv/test.csv'
+        self.model_path = '../model/bert-base-uncased'
+        self.output_dir = '../output'
+
+
+        #pet config
+        self.pet_repetitions = 3
+        self.pattern_ids = 1
+        self.pet_batch_size = 4
+
+
+def setup_training(config):
+    set_seed(config.seed)
+    device = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
+
+    # tensorboard set up
+    time_stamp = "{0:%Y-%m-%dT%H-%M-%S/}".format(datetime.now())
+    comment = f'bath_size={config.batch_size} lr={config.learning_rate} layers={config.transformer_num_hidden_layers}'
+    writer = SummaryWriter(log_dir='./data/tensorboard/' + time_stamp, comment=comment)
+
+    torch.backends.cudnn.enabled = True
+    torch.backends.cudnn.benchmark = True
+    return config, device, writer
+
+def prepare_data_loader(config, num_workers=1):
+    train_dataset = AgNewsDataset(config.train_data_path, config.model_path, config.pet_repetitions, config.max_seq_length)
+    train_data_iter = DataLoader(train_dataset, batch_size=config.batch_size, num_workers=num_workers, shuffle=True)
+    return train_data_iter
+
+def trainer():
+    config = Config()
+    data_iter = prepare_data_loader(config)
+    for w in data_iter:
+        print(w)
+        time.sleep(1)
+
+
+
+if __name__ == '__main__':
+    trainer()
